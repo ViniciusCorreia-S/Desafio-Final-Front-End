b3d460029ff936caa63537ce8397f44feda8e2e5
VINICIUS CORREIA <viniciuscorreia.couto@gmail.com>
2025-05-14 17:01:28 -0300

    home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { CarouselComponent } from '../../components/carousel/carousel.component';
+import { CatalogComponent } from '../../components/catalog/catalog.component';
 
 @Component({
   selector: 'app-home',
-  imports: [ HeaderComponent ],
+  imports: [ HeaderComponent , CarouselComponent , CatalogComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/pages/vehicles/vehicles.component.ts
+++ src/app/pages/vehicles/vehicles.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { SideBarComponent } from '../../components/side-bar/side-bar.component';
 
 @Component({
   selector: 'app-vehicles',
-  imports: [ SideBarComponent ],
+  imports: [  ],
   templateUrl: './vehicles.component.html',
   styleUrl: './vehicles.component.css'
 })
